498543add2406bfe7fb49aafeeb2d983242066c8
howard <howardnlee94@gmail.com>
2023-01-31 00:34:57 -0800

    added-post

--- Develop/controller/notesController.js
+++ Develop/controller/notesController.js
@@ -17,4 +17,53 @@ router.get("/", (req, res) => {
     });;
   });
 
+    
+  router.post("/", (req, res) => {
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+      if (err) {
+        res.status(500).send("oh no!");
+        throw err;
+      } else {
+        const notes = JSON.parse(data);
+        notes.push(req.body);
+        fs.writeFile("./db/db.json", JSON.stringify(notes, null, 4), (err) => {
+          if (err) {
+            res.status(500).send("oh no!");
+            throw err;
+          } else {
+            res.send("data added!");
+          }
+        });
+      }
+    });
+  });
+
+  router.delete("/:id", (req, res) => {
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+      if (err) {
+        res.status(500).send("oh no!");
+        throw err;
+      } else {
+        let notes = JSON.parse(data);
+        notes = notes.filter((notes1) => {
+          if (notes1.id == req.params.id) {
+            return false;
+          } else {
+            return true;
+          }
+        });
+        fs.writeFile("./db/db.json", JSON.stringify(notes, null, 4), (err) => {
+          if (err) {
+            res.status(500).send("oh no!");
+            throw err;
+          } else {
+            res.send("data deleted!");
+          }
+        });
+      }
+    });
+  });
+
+
+
   module.exports = router;
