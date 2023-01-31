f0bfffa8b42786168085a2aa98565a1fc0de53ad
howard <howardnlee94@gmail.com>
2023-01-31 14:12:15 -0800

    added-readme

--- controller/index.js
+++ controller/index.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router = express.Router();
 const path = require("path");
 
+
+// display the index.html file when they visit the home page which is localhost:3000
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
-  
+// display the note.html file when they visit localhost:3000/notes
 router.get("/notes", (req, res) => {
 res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
-
+// controller for all the notes file path
 const noteRoutes = require("./notesController");
 router.use("/api/notes",noteRoutes)
   
